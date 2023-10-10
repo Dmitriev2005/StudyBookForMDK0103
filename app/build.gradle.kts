9af02a744a180aea6e465f483633e25dd9fff678
Dmitriev2005 <121797189+Dmitriev2005@users.noreply.github.com>
2023-10-10 14:36:04 +0300

    Add icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.studybookformdk0103"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.studybookformdk0103"
